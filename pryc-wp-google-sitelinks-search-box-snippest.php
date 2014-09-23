cf140f52883c332a8cb83cb9937ed4527d71c86c
PRyCpl <PRyCpl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 21:22:15 +0000

    1st public
    git-svn-id: https://plugins.svn.wordpress.org/pryc-wp-google-sitelinks-search-box-snippest/trunk@995725 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pryc-wp-google-sitelinks-search-box-snippest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Plugin Name: PRyC WP: Google Sitelinks Search Box snippest
+ * Plugin URI: http://PRyC.pl
+ * Description: Plugin add to homepage code required to activate new Sitelinks Search Box @ Google search
+ * Author: PRyC
+ * Author URI: http://PRyC.pl
+ * Version: 1.0.2
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+ 
+/* Copyright PRyC (email: kontakt@PRyC.pl)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+ 
+ 
+/* CODE: */
+
+function pryc_wp_google_sitelinks_search_box() {
+	if(is_home()) {
+		$site_url = network_site_url('/');
+		#echo $site_url;
+		
+		?>
+		<!-- PRyC WordPress: Google Sitelinks Search Box snippest: -->
+		<script type="application/ld+json">
+		{
+		   "@context":"http://schema.org",
+		   "@type":"WebSite",
+		   "url": "<?php echo $site_url ?>",
+		   "potentialAction": {
+			 "@type":"SearchAction",
+			 "target": "<?php echo $site_url ?>?s={search_term}",
+			 "query-input": "required name=search_term"
+		   }            
+		}
+		</script>   	
+		<!-- /PRyC WordPress: Google Sitelinks Search Box snippest -->  	
+		<?php
+	}
+}
+add_action('wp_head', 'pryc_wordpress_google_sitelinks_search_box');
+
+
+/* END */
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== PRyC WP: Google Sitelinks Search Box snippest ===
+Contributors: PRyCpl
+Tags: WordPress, Google, Google search, Sitelinks Search Box
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin add to homepage code required to activate new Sitelinks Search Box @ Google search
+
+== Description ==
+Plugin add to homepage code required to activate new Sitelinks Search Box @ Google search.
+
+See more: https://developers.google.com/webmasters/richsnippets/sitelinkssearch
+
+== Installation ==
+1. Upload `pryc-wp-google-sitelinks-search-box-snippest` dir to the `/wp-content/plugins/` directory
+2. Activate the plugin through the \'Plugins\' menu in WordPress
+
+== Screenshots ==
+1. Default search before
+2. Default search after
+
+== Changelog ==
+1.0.1: First public ver.
